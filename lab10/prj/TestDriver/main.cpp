36fee92ff39ca6dfe35f4ae573eb617828b0398b
nazar <nazarlev20@gmail.com>
2024-04-17 02:30:44 +0300

    Performed the analysis and formulation of problems 10.1-10.3

---
+++ lab10/prj/TestDriver/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#include "ModulesKokish_.h"
+
+using namespace std;
+
+int main() {
+    //10.1
+    addAuthorInfo("input_1.txt","output_1.txt");
+    addAuthorInfo("input_2.txt","output_7.txt");
+    addAuthorInfo("input_3.txt","output_7.txt");
+    addAuthorInfo("input_4.txt","output_7.txt");
+    addAuthorInfo("input_5.txt","output_7.txt");
+    return 0;
+}
